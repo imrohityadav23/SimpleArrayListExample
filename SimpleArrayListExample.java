6fde805469ad155c58f344b39097ebe78b7b1bfc
ROHIT KUMAR <95360799+iamrohityadav23@users.noreply.github.com>
2022-08-29 14:35:54 +0530

    Add files via upload

---
+++ SimpleArrayListExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class SimpleArrayListExample{
+ public static void main(String []args){
+ArrayList<String> animals = new ArrayList<String>();
+animals.add("Lion");
+animals.add("Tiger");
+animals.add("Cat");
+animals.add("Dog");
+System.out.println(animals);
+}
+}
